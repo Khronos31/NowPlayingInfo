34128ab2dde32fd0bf63faf982ba48f115ee025f
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2022-02-02 16:55:30 +0900

    いろいろ

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
+PACKAGE_VERSION = $(THEOS_PACKAGE_BASE_VERSION)
 TARGET := iphone:clang:latest:7.0
 ARCHS = arm64 arm64e
 INSTALL_TARGET_PROCESSES = SpringBoard
-#FINALPACKAGE = 1
+FINALPACKAGE = 1
 
 include $(THEOS)/makefiles/common.mk
 
@@ -25,3 +26,7 @@ $(TOOL_NAME)_INSTALL_PATH = /usr/local/bin
 include $(THEOS_MAKE_PATH)/tweak.mk
 include $(THEOS_MAKE_PATH)/library.mk
 include $(THEOS_MAKE_PATH)/tool.mk
+
+before-package::
+	@mkdir -p $(THEOS_STAGING_DIR)/usr/local/include
+	@cp $(THEOS_BUILD_DIR)/NowPlayingInfo.h $(THEOS_STAGING_DIR)/usr/local/include
--- main.mm
+++ main.mm
@@ -1,6 +1,16 @@
 #import <Foundation/Foundation.h>
 #import "NowPlayingInfo.h"
 
+void print_usage(int argc, char *argv[], char *envp[]) {
+  fprintf(stderr, "Usage: %s [title|artist|album|playingapp]", argv[0]);
+  exit(1);
+}
+
 int main(int argc, char *argv[], char *envp[]) {
-  NSLog(@"%@, %@, %@, %@, %@",[NowPlayingInfo nowPlayingApplication], [NowPlayingInfo title], [NowPlayingInfo artist], [NowPlayingInfo album], [NowPlayingInfo artwork]);
+  const char *title = [NowPlayingInfo title].UTF8String;
+  const char *artist = [NowPlayingInfo artist].UTF8String;
+  const char *album = [NowPlayingInfo album].UTF8String;
+  const char *playingApp = [NowPlayingInfo nowPlayingApplication].UTF8String;
+  printf("Title: %s\nArtist: %s\nAlbum: %s\nPlayingApp: %s\n", title, artist, album, playingApp);
+  return 0;
 }
